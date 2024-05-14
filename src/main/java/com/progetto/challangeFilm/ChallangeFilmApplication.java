0efe30f2197e8b45fb9fe4c57bae970c9a1e6d38
simone1222 <114957126+simone1222@users.noreply.github.com>
2024-05-14 10:13:22 +0200

    commit

---
+++ src/main/java/com/progetto/challangeFilm/ChallangeFilmApplication.java
@@ -0,0 +1,13 @@
+package com.progetto.challangeFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progetto/challangeFilm/ChallangeFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto.challangeFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
